ead8cb3db3a05b12533275fd5053f30aa4ed18ca
Maya <maya.hubbard@outlook.com>
2023-08-17 18:23:17 -0700

    Learning how to Remove an Event Listener

--- app.js
+++ app.js
@@ -65,7 +65,9 @@ for (let card of cards) {
 
 /**END OF CREATING CONTENT WTIH JAVASCRIPT SECTION */
 
-/**EXAMPLE: ADD AN EVENT LISTENER TO THE PROJECT */
+
+
+/** ADD AN EVENT LISTENER TO THE PROJECT */
 
 // document.addEventListener("click", function() {
 //   const mainHeading = document.querySelector("h1");
@@ -74,15 +76,73 @@ for (let card of cards) {
 
 /** EXERCISE: ADD A LISTENER */
 
-const header = document.querySelector("h1");
-const originalText = header.textContent;
-const updatedText = "Build an AR App";
+// const header = document.querySelector("h1");
+// const originalText = header.textContent;
+// const updatedText = "Build an AR App";
 
-header.addEventListener("mouseover", function() {
-  header.textContent = updatedText;
-});
+// function hover() {
+//   header.textContent = updatedText;
+// }
 
-header.addEventListener("mouseout", function() {
-  header.textContent = originalText;
-});
+// function away() {
+//   header.textContent = originalText;
+// }
 
+// header.addEventListener("mouseover", hover);
+// header.addEventListener("mouseout", away);
+
+
+//EXERCISE: REMOVE AN EVENT LISTENER
+// header.removeEventListener("mouseover", hover);
+// header.removeEventListener("mouseout", away);
+
+
+/** EXPLANATION: ADD/REMOVE AN EVENT LISTENER 
+ * 
+ * In order to remove an event listener, the same function that was passed into the addEventListener() must be passed into the remmoveEventListener().
+ * 
+ * EXAMPLE OF WRONG WAY:
+ * 
+ * Inserting this function into the add/remove events will point to two different locations and the remove will fail:
+ * 
+ * function() { 
+ * header.textContent = originalText;
+ * }
+ * 
+ * function() {
+ * header.textContent = updated text:
+ * }
+ * 
+ * 
+ * In the example above, the statements that are executed by adding the function to both the add/remove listeners and need to be converted into a function that can be passed into the add/remove function parameters.
+ * 
+ * Passing the statements as above creates an object. Using the statement a second time will create a new object instead of pointing to the one that was added.
+ * 
+ * I created functions for each event listener and replaced the statements to ensure each listener points to the same object.
+ * 
+*/
+
+
+
+
+/** EXERCISE: REMOVE AN EVENT LISTENER
+ *    Create a one-time use event listener
+ */
+
+document.addEventListener("click", disappear);
+// document.removeEventListener("click", disappear);
+const hero = document.querySelector(".hero__module");
+  
+
+
+function disappear() {
+  //removes last element (child) from the hero--module element
+  const lastCard = hero.lastElementChild;
+  hero.removeChild(lastCard);
+  console.log("disappeared")
+}
+
+function gone() {
+  //remove event listener after it's been triggered
+  
+}
